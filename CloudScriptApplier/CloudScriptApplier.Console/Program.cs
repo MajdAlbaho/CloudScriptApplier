da8585c2cfe50b91c28273208c523256545ca35f
edward-shami <48517272+edward-shami@users.noreply.github.com>
2019-03-13 14:48:38 +0200

    check connection

--- CloudScriptApplier/CloudScriptApplier.Console/Program.cs
+++ CloudScriptApplier/CloudScriptApplier.Console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,27 @@ namespace CloudScriptApplier.Console
 {
     class Program
     {
-        static void Main(string[] args)
+          static void Main(string[] args)
         {
+
+            //Check For Internet
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                IAsyncResult result = socket.BeginConnect("www.google.com", 80, null, null);
+                //5 sec timeout
+                bool success = result.AsyncWaitHandle.WaitOne(5000, true);
+                if (!success)
+                {
+                    throw new ApplicationException("Failed to connect server.");
+                }
+            }
+            finally
+            {
+                socket.Close();
+            }
         }
+
+
     }
 }
