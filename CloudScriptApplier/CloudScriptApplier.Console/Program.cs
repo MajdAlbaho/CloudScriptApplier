aace55f64048dfbae4d8d512975aca1d49046f6d
Majd Albaho <Majd-Albaho@outlook.com>
2019-03-17 12:12:58 +0200

    test access shared folder

--- CloudScriptApplier/CloudScriptApplier.Console/Program.cs
+++ CloudScriptApplier/CloudScriptApplier.Console/Program.cs
@@ -3,7 +3,9 @@ using CloudScriptApplier.Db;
 using Ninject;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
 using System.Text;
@@ -14,6 +16,13 @@ namespace CloudScriptApplier.Console
     class Program
     {
         static void Main(string[] args) {
+
+            NetworkCredential theNetworkCredential = new NetworkCredential(@"Temo", "P@ssw0rd");
+            CredentialCache theNetCache = new CredentialCache();
+            theNetCache.Add(new Uri(@"\\82.137.255.183\Fofo"), "Basic", theNetworkCredential);
+
+            var files = Directory.GetFiles(@"\\172.16.11.23\Fofo");
+
             var kernal = new StandardKernel();
             kernal.Load(Assembly.GetExecutingAssembly());
 
